1f9d641d3f48e5f803570412a08034594019361a
Kurt Barry <kurt.m.barry@gmail.com>
2021-01-01 22:01:55 -0500

    COVER setup done

--- src/DhCover20201228.t.sol
+++ src/DhCover20201228.t.sol
@@ -4,10 +4,32 @@ pragma solidity ^0.7.4;
 import "ds-test/test.sol";
 
 import "./Blacksmith.sol";
+import "./COVER.sol";
+
+interface Hevm {
+    function warp(uint256) external;
+}
 
 contract DhCover20201228Test is DSTest {
 
+    // CHEAT_CODE = 0x7109709ECfa91a80626fF3989D68f67F5b1DD12D
+    bytes20 constant CHEAT_CODE =
+        bytes20(uint160(uint256(keccak256('hevm cheat code'))));
+
+    Hevm hevm;
+
+    COVER coverToken;
+    Blacksmith blacksmith;
+
     function setUp() public {
+        hevm = Hevm(address(CHEAT_CODE));
+
+        coverToken = new COVER();
+        address governance = address(this);
+        address treasury = address(0x1);
+        blacksmith = new Blacksmith(address(coverToken), governance, treasury);
+        hevm.warp(1605830400);  // 11/20/2020 12am UTC
+        coverToken.release(treasury, address(0x2), address(blacksmith), address(0x3));
     }
 
     function test_basic_sanity() public {
